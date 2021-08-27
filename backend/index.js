57637a70e6e3b3be86e39eec39b3d1d00ad5b28e
Anthony Ahumada <locopala99@gmail.com>
2021-08-27 08:18:01 +0200

    More js

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.listen(process.env.PORT,()=>console.log("Backend Server Running  Ok On Port",process.env.PORT))
