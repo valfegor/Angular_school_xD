c4ee85e98fdb19216a50685301cd390eddb32912
Anthony Ahumada <locopala99@gmail.com>
2021-08-27 09:51:04 +0200

    more js

--- frontend/src/app/home/register-student/register-student.component.ts
+++ frontend/src/app/home/register-student/register-student.component.ts
@@ -1,29 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EstudentService } from '../../services/estudent.service';
 import {
   MatSnackBar,
   MatSnackBarHorizontalPosition,
   MatSnackBarVerticalPosition,
 } from '@angular/material/snack-bar';
-
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-register-student',
   templateUrl: './register-student.component.html',
-  styleUrls: ['./register-student.component.css']
+  styleUrls: ['./register-student.component.css'],
 })
 export class RegisterStudentComponent implements OnInit {
-  public registerData:any;
+  public registerData: any;
   public message: string;
   public horizontalPosition: MatSnackBarHorizontalPosition = 'end';
   public VerticalPosition: MatSnackBarVerticalPosition = 'top';
+  public durationInseconds: number;
   constructor(
-    private _snackbar: MatSnackBar
-  ) { 
+    private _snackbar: MatSnackBar,
+    private _router: Router,
+    private _estudentService: EstudentService
+  ) {
     this.message = '';
     this.registerData = {};
+    this.durationInseconds = 2;
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  registerEstudent(){
+    if(!this.registerData.name || !this.registerData.email || !this.registerData.adress || !this.registerData.parentName || !this.registerData.parentEmail || !this.registerData.estudentPhone || this.registerData.parentPhone || !this.registerData.password){
+      console.log('Failed the process the data its incomplete');
+      this.message = 'Failed the process the data its incomplete';
+      this.openSnackBarError();
+      this.registerData = {};
+    }
+    else{
+      this._estudentService.registerEstudent(this.registerData).subscribe(
+        (res)=>{
+          
+        },
+        (err)=>{
+
+        }
+      )
+    }
+  }
 }
