b3ad4aced7717eff47ad2679ccdb27be1ee284eb
Anthony Ahumada <locopala99@gmail.com>
2021-08-27 09:38:24 +0200

    more js

--- frontend/src/app/services/estudent.service.ts
+++ frontend/src/app/services/estudent.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http";
+import {environment  } from "../../environments/environment";
 @Injectable({
   providedIn: 'root'
 })
 export class EstudentService {
+  private env:string;
+  constructor(private _http:HttpClient) {
+    this.env = environment.APP_URL;
+   }
 
-  constructor() { }
+   registerEstudent(estudent:any){
+    return this._http.post<any>(this.env+'estudent/registerEstudent',estudent);
+   }
+
 }
